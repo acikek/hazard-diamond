ec48a67f89686dbede856e88abed5a91d798de2a
acikek <spiralixp@gmail.com>
2023-05-08 14:44:30 -0700

    g

---
+++ src/main/java/com/acikek/hdiamond/api/block/BlockWithHazardData.java
@@ -0,0 +1,10 @@
+package com.acikek.hdiamond.api.block;
+
+import net.minecraft.block.Block;
+
+public class BlockWithHazardData extends Block {
+
+    public BlockWithHazardData(Settings settings) {
+        super(settings);
+    }
+}
